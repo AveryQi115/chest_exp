287427d4e16b73b74f36ae54e08cf43773b32c2a
Nicola115 <2225992901@qq.com>
2021-01-31 03:53:50 -0500

    change main_lincls.py

---
+++ dataset.py
@@ -0,0 +1,81 @@
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+from PIL import Image
+import random, cv2
+import os
+import numpy as np
+import json
+
+class CFG():
+    def __init__(self):
+        self.DATASET_UNCERTAIN = "U-positive"
+        self.INPUT_SIZE = (320,320)
+        self.DATASET_TRAIN_JSON = "chexpert_moco_train.json"
+        self.DATASET_VALID_JSON = "chexpert_moco_valid.json"
+
+class CheXpert(Dataset):
+    def __init__(self, mode='train', transform=None,cfg=CFG()):
+        super().__init__()
+        random.seed(0)
+        self.mode = mode
+        self.transform = transform
+        self.cfg = cfg
+        self.input_size = cfg.INPUT_SIZE
+
+        if self.mode == "train":
+            print("Loading train data ...")
+            self.json_path = cfg.DATASET_TRAIN_JSON
+        elif "valid" in self.mode:
+            print("Loading valid data ...")
+            self.json_path = cfg.DATASET_VALID_JSON
+
+        with open(self.json_path, "r") as f:
+            self.all_info = json.load(f)
+        self.num_classes = self.all_info["num_classes"]
+        self.data = self.all_info["annotations"]
+        print("Contain {} images of {} classes".format(len(self.data), self.num_classes))
+        
+        transform_uncertain = [{"nan":0,0:0,1:1,-1:0},
+                                {"nan":0,0:0,1:1,-1:1},
+                                {"nan":0,0:0,1:1,-1:-1}]
+        if cfg.DATASET_UNCERTAIN == "U-positive":
+            self.transform_dict = transform_uncertain[1]
+        elif cfg.DATASET_UNCERTAIN == "U-negative":
+            self.transform_dict == transform_uncertain[0]
+        elif cfg.DATASET_UNCERTAIN == "U-ignore":
+            self.transform_dict == transform_uncertain[2]
+
+
+    def __getitem__(self, index):
+        now_info = self.data[index]
+        img = Image.open(now_info['path'])
+        image = self.transform(img)
+        label = self._get_label(now_info)
+        return image, label
+
+
+    def _get_label(self,now_info):
+        label = []
+        for key in now_info.keys():
+            if key in ['No Finding','Enlarged Cardiomediastinum','Cardiomegaly','Lung Opacity','Lung Lesion','Edema','Consolidation','Pneumonia','Atelectasis','Pneumothorax','Pleural Effusion','Pleural Other','Fracture','Support Devices']:
+                label.append(self.transform_dict[now_info[key]])
+        label = np.array(label)
+        assert label.shape == (self.num_classes,)
+        return label
+
+
+    def get_annotations(self):
+        clean_anno = []
+        for anno in self.data:
+            clean_anno_dict=dict()
+            for key in anno.keys():
+                if key not in ['path','Sex','Age','Frontal/Lateral','AP/PA']:
+                    clean_anno_dict[key] = self.transform_dict[anno[key]]
+            clean_anno.append(clean_anno_dict)
+        return clean_anno
+
+    def get_num_classes(self):
+        return self.num_classes
+
+    def __len__(self):
+        return len(self.data)
---
+++ loss.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import numpy as np
+from torch.autograd import Variable
+
+
+def get_category_list(annotations, num_classes):
+    num_list = [0] * num_classes
+    cat_list = []
+    print("Weight List has been produced")
+    
+    negative_list = dict()
+    positive_list = dict()
+    cat_list = []
+
+    # initialize dict
+    for key in annotations[0].keys():
+        positive_list[key] = 0
+        negative_list[key] = 0
+
+    for anno in annotations:
+        cat = []
+        for key in anno.keys():
+            # ipdb.set_trace()
+            if key not in ['path','Sex','Age','Frontal/Lateral','AP/PA'] and type(anno[key])!=str and anno[key] >= 1:
+                positive_list[key] += 1
+                cat.append(key)
+            
+            if key not in ['path','Sex','Age','Frontal/Lateral','AP/PA'] and type(anno[key])!=str and anno[key] == 0:
+                negative_list[key] += 1
+        cat_list.append(cat)
+    
+    for i, key in enumerate(negative_list.keys()):
+        assert negative_list[key] != 0,f"negative_list[{key}]=0 error"
+        num_list[i] = positive_list[key]/negative_list[key]
+    return num_list,cat_list
+
+
+class BCEWithLogitsLoss(nn.Module):
+    def __init__(self, num_class_list):
+        super(BCEWithLogitsLoss, self).__init__()
+        self.num_class_list = num_class_list
+
+    def forward(self, args, output, target):
+        # output.size = (batchsize, num_classes)
+        # pos_weight.size = (num_classes,)
+        num_classes = output.shape[1]
+        
+        # TODO: num_class_list
+        pos_weight = torch.from_numpy(
+            np.array(self.num_class_list,
+                     dtype=np.float32)).cuda(args.gpu).type_as(output)
+
+        target = target.cuda(args.gpu).type_as(output)
+        
+        loss_list = []
+        for i in range(num_classes):
+            target_i = target[:, i].view(-1)
+            loss_list.append(F.binary_cross_entropy_with_logits(output[:,i].view(-1), target_i,pos_weight[i]))
+        loss = torch.sum(torch.stack(loss_list))
+        return loss
